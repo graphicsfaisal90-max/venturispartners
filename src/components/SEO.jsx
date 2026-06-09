17945b7696b136601aaf3beb71bafb1c05e843d8
Faisal <graphicsfaisal90-max@users.noreply.github.com>
2026-06-09 22:59:03 +0500

    Add og:image and twitter:image meta tags for Google search result preview

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -18,6 +18,7 @@ export default function SEO({ title, description, keywords = [] }) {
   const fullTitle = title ? `${title} | ${siteName}` : siteName
   const allKeywords = [...defaultKeywords, ...keywords].join(', ')
   const url = 'https://venturispartner.com'
+  const image = `${url}/3232.png`
 
   return (
     <Helmet>
@@ -30,11 +31,15 @@ export default function SEO({ title, description, keywords = [] }) {
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="469" />
+      <meta property="og:image:height" content="436" />
       <meta property="og:site_name" content={siteName} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={fullTitle} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
 
       <meta name="robots" content="index, follow" />
       <meta name="googlebot" content="index, follow" />
